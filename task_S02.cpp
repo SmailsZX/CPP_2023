e3b5fdabce9619456d707d58df49842d71a09da9
SmailsZX <164626956+SmailsZX@users.noreply.github.com>
2025-10-13 16:25:22 +0800

    Add files via upload

---
+++ task_S02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Создаем 4 переменные разных типов
+    int f = 42;
+    int g = 12;
+    cout << "ДЕЛЕНИЕ (нацело и с остатком):" << endl;
+    cout << f << " / " << g << " = " << f / g << endl;        
+    cout << f << " % " << g << " = " << f % g << endl;  
+    
+    return 0;
+}
---
+++ task_S11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i <= 100; i += 3) {
+        cout << i << "\t";
+    }
+    return 0;
+}
